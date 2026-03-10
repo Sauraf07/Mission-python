0784e58b1be45e35f5356c9cb232f1d29487c0a2
Sauraf Kumar <saurafsps7631@gmail.com>
2026-03-10 20:37:52 +0530

    day 8

---
+++ python/day 8oop.py
@@ -0,0 +1,30 @@
+# class Student:
+#     def __init__(self,name,id):
+#         self.name = name
+#         self.id = id
+# s1 = Student("Priyam",1)
+# s2 = Student("Saurav",2)
+# print(s1.name,s1.id)
+# print(s2.name,s2.id)
+
+# -------------------------------------------
+# Class Vereiable
+# class College:
+#     college_name = "Dragons International COllege"
+#     def __init__(self,name, id):
+#         self.name = name
+#         self.id = id
+# College.college_name = "Dragons"
+# c1 = College("Saurav",1)
+# c2 = College("Priyam",2)
+# print("My name is",c1.name,"my id is ",c1.id,"and my college name is,",c1.college_name)        
+
+# ----------------------------------------------------------------------------------------
+# class Car:
+#     wheels = 4 # Class Variable
+#     def __init__(self, brand):
+#         self.brand = brand
+# c1 = Car("BMW")
+# c2 = Car("Tesla")
+# print(c1.wheels)
+# print(c2.wheels)
