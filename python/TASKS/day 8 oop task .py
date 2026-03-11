944b33244cd8f3839ed74d58483ae547aa0ddcd2
Sauraf Kumar <saurafsps7631@gmail.com>
2026-03-11 21:18:11 +0530

    day 9

---
+++ python/TASKS/day 8 oop task .py
@@ -0,0 +1,35 @@
+# class Student:
+#     School_name = "Abc School"
+#     def __init__(self,name,marks):
+#         self.name = name 
+#         self.marks = marks
+# s1 = Student("Saurav",85)
+# print("My name is ",s1.name,"And my marks is",s1.marks,"and i am from ",s1.School_name)
+
+# ----------------------------------------------------------------------------------------
+# class Employee:
+#     company_name = "Deagons Pvt"
+#     def __init__(self,name,salary):
+#         self.name = name
+#         self.salary = salary
+# e1 = Employee("Saurav",5000)
+# print(e1.name,e1.salary,e1.company_name)
+    
+# ------------------------------------------------------------
+# class Player:
+#     game_name = "PUBG"
+#     def __init__(self,name, health):
+#         self.name = name 
+#         self.health = health
+# p1 = Player("Saurav","Good")
+# print(p1.name,p1.health,p1.game_name)        
+
+# ----------------------------------------------------------
+# class Car:
+#     wheels = 4
+#     def __init__(self,brand,price):
+#         self.brand = brand
+#         self.price = price
+# c1 = Car("Tesla",5000000)
+# print(c1.brand,c1.price,c1.wheels)
+    
